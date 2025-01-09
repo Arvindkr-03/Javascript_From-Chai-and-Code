285711d5ff6f842a66983b887c402b5fa4742b89
Arvindkr-03 <arvind.bcastudent22.8111@cimage.in>
2025-01-09 21:52:42 +0530

    not recived

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const req=require(".script2.js")
 const app = express()
+console.log(req);
+
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
 app.listen(3000)
+
