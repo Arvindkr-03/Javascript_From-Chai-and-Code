cdf8b8c7c96be9f9ff56114d0d123ffc18eb5dda
Arvindkr-03 <arvind.bcastudent22.8111@cimage.in>
2025-01-09 21:52:15 +0530

    used module

---
+++ Js Project1/Backend/script.js
@@ -0,0 +1,6 @@
+// const back = require("./script2");
+// console.log(back);
+
+var joke=require('one-liner-joke');
+
+console.log( joke.getRandomJoke());
---
+++ Js Project1/Backend/script2.js
@@ -0,0 +1,4 @@
+var a=58;
+console.log("* Welcome to nodejs.......");
+console.log(a);
+module.exports =a;
